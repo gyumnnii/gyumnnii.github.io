8a3965e0f1175dde452dcb1b7a25af3242f61a3c
gyumnnii <gogumasi3@gmail.com>
2021-03-12 22:08:45 +0900

    Add blog material-ui blog template

--- src/router/RouterContainer.tsx
+++ src/router/RouterContainer.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { createStore } from 'redux';
-import Home from '../pages/Home/Home';
+// import Home from '../pages/Home/Home';
 import Signin from '../pages/Signin/Signin';
 import Signup from '../pages/Signup/Signup';
 import rootReducer from '../store';
+import Blog from '../components/Blog';
 
 const store = createStore(rootReducer);
 
@@ -13,7 +14,7 @@ const RouterContainer = (): JSX.Element => {
   return (
     <Provider store={store}>
       <BrowserRouter>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/" component={Blog} />
         <Route path="/signin" component={Signin} />
         <Route path="/signup" component={Signup} />
       </BrowserRouter>
