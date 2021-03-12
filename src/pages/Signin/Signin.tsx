f4e1d782b7a29502f235377e8dd225b4a2132db8
gyumnnii <gogumasi3@gmail.com>
2021-03-12 22:10:34 +0900

    Remove reactstrap and components from project

--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import LoginButton from '../../components/Button/LoginButton';
 
 const Signin = (): JSX.Element => {
   return (
     <>
       <div>Hello Signin Page!</div>
-      <LoginButton />
     </>
   );
 };
